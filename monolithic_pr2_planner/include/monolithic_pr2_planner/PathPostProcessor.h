05936fe27de4cff93d931f5c92486137975d8589
Victor Hwang <vchwang@andrew.cmu.edu>
2014-01-07 12:42:51 -0500

    separated path post processing to its own class

---
+++ monolithic_pr2_planner/include/monolithic_pr2_planner/PathPostProcessor.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <monolithic_pr2_planner/MotionPrimitives/MotionPrimitive.h>
+#include <monolithic_pr2_planner/HashManager.h>
+#include <monolithic_pr2_planner/CollisionSpaceMgr.h>
+#include <vector>
+
+namespace monolithic_pr2_planner {
+    struct FullBodyState {
+        std::vector<double> base;
+        std::vector<double> left_arm;
+        std::vector<double> right_arm;
+    };
+    class PathPostProcessor {
+        public:
+            PathPostProcessor(HashManagerPtr hash_mgr, CSpaceMgrPtr cspace_mgr);
+            std::vector<FullBodyState> reconstructPath(std::vector<int> state_ids,
+                                                       GoalState& goal_state,
+                                                       std::vector<MotionPrimitivePtr> mprims);
+        private:
+            std::vector<FullBodyState> getFinalPath(const vector<int>& state_ids,
+                                            const vector<TransitionData>& transition_states,
+                                            GoalState& goal_state);
+            bool findBestTransition(int start_id, int end_id, TransitionData& t_data,
+                                    std::vector<MotionPrimitivePtr> mprims);
+            void visualizeFinalPath(std::vector<FullBodyState> path);
+            FullBodyState createFBState(const RobotState& robot);
+
+            CSpaceMgrPtr m_cspace_mgr;
+            HashManagerPtr m_hash_mgr;
+    };
+}
